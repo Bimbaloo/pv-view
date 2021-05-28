fbf549dfa69b4bdb6874ad6a4930661a84d12b0d
mazao <1030102244@qq.com>
2021-05-28 09:02:25 +0800

    计划投入数为0，整行变灰

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -180,7 +180,7 @@
             <template slot-scope="props">
               <div
                 :class="{
-                  grey: props.row[col.prop] == '0' && col.prop == 'jhtrs'
+                  grey: props.row['jhtrs'] == '0'
                 }"
               >
                 {{ props.row[col.prop] }}
